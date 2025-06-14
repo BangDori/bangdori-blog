e4f6e5cf3eca002a0c6de6e466efdf6f5e3d004d
BangDori <qudwns8865@naver.com>
2025-06-14 19:58:38 +0900

    about 추가

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,65 @@
+import Image from 'next/image';
+import { Github, Instagram, Linkedin, Mail } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+
+const socialLinks = [
+  {
+    icon: Github,
+    href: 'https://github.com/bangdori',
+  },
+  {
+    icon: Linkedin,
+    href: 'https://www.linkedin.com/in/bangdori/',
+  },
+  {
+    icon: Instagram,
+    href: 'https://www.instagram.com/joooon2_/',
+  },
+  {
+    icon: Mail,
+    href: `mailto:bangdori@gmail.com?subject=제목을 입력해주세요&body=${encodeURIComponent(
+      '안녕하세요, 블로그를 보고 연락드립니다.\n\n문의 내용:\n'
+    )}`,
+  },
+];
+
 export default function About() {
-  return <div>준비중인 페이지입니다.</div>;
+  return (
+    <section className="container">
+      <div className="flex flex-col items-center gap-4">
+        <Image
+          src="/profile.jpg"
+          alt="프로필 사진"
+          width={160}
+          height={160}
+          className={cn(
+            'h-40 w-40 rounded-full object-cover',
+            'shadow-[8px_8px_24px_0_rgba(0,180,216,0.18),_-8px_-8px_24px_0_rgba(200,240,255,0.7)]'
+          )}
+          priority
+        />
+        <p className="text-md font-bold">안녕하세요, 강병준입니다</p>
+        <p className="text-muted-foreground text-center text-base font-medium">
+          혼자보단 함께하는 게 더 즐거워요.
+          <br />
+          복잡한 문제도 함께라면 해결할 수 있기에,
+          <br />
+          서로를 이해하며 조화롭게 빛나는 서비스를 만들고 싶어요.
+        </p>
+      </div>
+
+      <div className="mt-12">
+        <div className="flex justify-center gap-2">
+          {socialLinks.map((item, index) => (
+            <Button key={index} variant="ghost" className="bg-primary/10" size="icon" asChild>
+              <a href={item.href} target="_blank" rel="noopener noreferrer">
+                <item.icon className="h-4 w-4" />
+              </a>
+            </Button>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
--- components/layouts/Footer.tsx
+++ components/layouts/Footer.tsx
@@ -1,42 +1,9 @@
-import { Github, Instagram, Linkedin, Mail } from 'lucide-react';
-import { Button } from '../ui/button';
-
-const socialLinks = [
-  {
-    icon: Github,
-    href: 'https://github.com/bangdori',
-  },
-  {
-    icon: Linkedin,
-    href: 'https://www.linkedin.com/in/bangdori/',
-  },
-  {
-    icon: Instagram,
-    href: 'https://www.instagram.com/joooon2_/',
-  },
-  {
-    icon: Mail,
-    href: `mailto:bangdori@gmail.com?subject=제목을 입력해주세요&body=${encodeURIComponent(
-      '안녕하세요, 블로그를 보고 연락드립니다.\n\n문의 내용:\n'
-    )}`,
-  },
-];
-
 export default function Footer() {
   return (
     <footer className="bg-background top-0 z-50 border-b">
       <div className="container flex h-[var(--header-height)] items-center px-4">
-        <div className="flex w-full items-center justify-between">
-          <div className="flex justify-center gap-2">
-            {socialLinks.map((item, index) => (
-              <Button key={index} variant="ghost" className="bg-primary/10" size="icon" asChild>
-                <a href={item.href} target="_blank" rel="noopener noreferrer">
-                  <item.icon className="h-4 w-4" />
-                </a>
-              </Button>
-            ))}
-          </div>
-          <p className="text-muted-foreground text-sm">© 2025 강병준. All rights reserved.</p>
+        <div className="flex w-full items-baseline justify-between">
+          <p className="text-muted-foreground text-xs">© 2025 강병준. All rights reserved.</p>
         </div>
       </div>
     </footer>
