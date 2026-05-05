04a879d83a98672bf3ade1cf45f7923e6ced843c
bangdori <qudwns8865@naver.com>
2026-05-05 22:54:34 +0900

    refactor: packages/db 패키지 삭제

--- packages/db/src/migration/1777971403663-Init.ts
+++
@@ -1,7 +0,0 @@
-import type { MigrationInterface, QueryRunner } from 'typeorm';
-
-export class Init1777971403663 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
