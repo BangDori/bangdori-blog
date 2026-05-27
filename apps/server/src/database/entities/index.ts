1a57bc8d93a2c7aab385bb147fa10fc974fe71b9
bangdori <qudwns8865@naver.com>
2026-05-27 23:58:41 +0900

    feat(server): users 테이블과 User 엔티티 추가
    - admin/CMS 로그인 계정용 users 테이블 (email unique, argon2 password_hash, role)
    - 본인 1인 admin 운영 정책상 role 기본값 'admin'
    - entities/index.ts 의 entities 배열에 User 등록

--- apps/server/src/database/entities/index.ts
+++ apps/server/src/database/entities/index.ts
@@ -1,4 +1,5 @@
 import { ExternalPost } from '@database/entities/external-post.entity';
 import { Post } from '@database/entities/post.entity';
+import { User } from '@database/entities/user.entity';
 
-export const entities = [Post, ExternalPost];
+export const entities = [Post, ExternalPost, User];
